9cb630ce21af4f41e936a12a31ae642fc6d6acd0
bastej <siatkasebastian@gmail.com>
2020-09-09 22:24:48 +0200

    Start refactoring with using abstract classes

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,12 @@
 import fs from "fs";
-import { dateStringToDate } from "./utils";
-import { MatchResult } from "./MatchResult";
+import { MatchData } from "./MatchReader";
 
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-export class CsvFileReader {
+export abstract class CsvFileReader {
   data: MatchData[] = [];
 
   constructor(public filepath: string) {}
+
+  abstract mapRow(row: string[]): MatchData;
 
   read(): void {
     this.data = fs
@@ -17,18 +17,6 @@ export class CsvFileReader {
       .map((row: string): string[] => {
         return row.split(",");
       })
-      .map(
-        (row: string[]): MatchData => {
-          return [
-            dateStringToDate(row[0]),
-            row[1],
-            row[2],
-            parseInt(row[3]),
-            parseInt(row[4]),
-            row[5] as MatchResult,
-            row[6],
-          ];
-        }
-      );
+      .map(this.mapRow);
   }
 }
