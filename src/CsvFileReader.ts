dea1507aab6113282821d5c2e2ba47e788402ee7
bastej <siatkasebastian@gmail.com>
2020-09-09 23:17:10 +0200

    Refactoring with use of interfaces

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,11 +1,8 @@
 import fs from "fs";
-
-export abstract class CsvFileReader<T> {
-  data: T[] = [];
+export class CsvFileReader {
+  data: string[][] = [];
 
   constructor(public filepath: string) {}
-
-  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
@@ -15,7 +12,6 @@ export abstract class CsvFileReader<T> {
       .split("\n")
       .map((row: string): string[] => {
         return row.split(",");
-      })
-      .map(this.mapRow);
+      });
   }
 }
