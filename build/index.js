63019d2556b9ea5d8fd11b401787a1680a53e204
bastej <siatkasebastian@gmail.com>
2020-09-09 23:56:06 +0200

    Js compiling into TypeScript

--- build/index.js
+++ build/index.js
@@ -1,24 +1,18 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-var fs_1 = __importDefault(require("fs"));
-var matches = fs_1.default
-    .readFileSync("football.csv", {
-    encoding: "utf-8",
-})
-    .split("\n")
-    .map(function (row) {
-    return row.split(",");
-});
+var MatchReader_1 = require("./MatchReader");
+var CsvFileReader_1 = require("./CsvFileReader");
+var MatchResult_1 = require("./MatchResult");
+var csvFileReader = new CsvFileReader_1.CsvFileReader("football.csv");
+var matchReader = new MatchReader_1.MatchReader(csvFileReader);
+matchReader.load();
 var manUnitedWins = 0;
-for (var _i = 0, matches_1 = matches; _i < matches_1.length; _i++) {
-    var match = matches_1[_i];
-    if (match[1] === "Man United" && match[5] === "H") {
+for (var _i = 0, _a = matchReader.matches; _i < _a.length; _i++) {
+    var match = _a[_i];
+    if (match[1] === "Man United" && match[5] === MatchResult_1.MatchResult.HomeWin) {
         manUnitedWins++;
     }
-    else if (match[2] === "Man United" && match[5] === "A") {
+    else if (match[2] === "Man United" && match[5] === MatchResult_1.MatchResult.AwayWin) {
         manUnitedWins++;
     }
 }
