79505385873a86c136c23c41e01acc21c96b613e
bastej <siatkasebastian@gmail.com>
2020-09-09 22:38:21 +0200

    Using generics

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,12 +1,11 @@
 import fs from "fs";
-import { MatchData } from "./MatchReader";
 
-export abstract class CsvFileReader {
-  data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+  data: T[] = [];
 
   constructor(public filepath: string) {}
 
-  abstract mapRow(row: string[]): MatchData;
+  abstract mapRow(row: string[]): T;
 
   read(): void {
     this.data = fs
