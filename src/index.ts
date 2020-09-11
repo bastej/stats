8210afc0ddba7ede91d295f0e471308416bee2fa
bastej <siatkasebastian@gmail.com>
2020-09-11 22:56:29 +0200

    New type of report class implementation

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,13 @@ import { MatchResult } from "./MatchResult";
 import { Summary } from "./Summary";
 import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const manUnitedSummary = new Summary(new WinsAnalysis("Man United"), new ConsoleReport());
+const manUnitedSummary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
 
 manUnitedSummary.buildAndPrintReport(matchReader.matches);
