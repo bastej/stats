6007526a8d179b68d53ae9d692f124487484a459
bastej <siatkasebastian@gmail.com>
2020-09-11 23:12:49 +0200

    Creating shortcut for class creating

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,12 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
-import { MatchResult } from "./MatchResult";
 import { Summary } from "./Summary";
-import { WinsAnalysis } from "./analyzers/WinsAnalysis";
-import { ConsoleReport } from "./reportTargets/ConsoleReport";
-import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader("football.csv");
 
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const manUnitedSummary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
+const manUnitedSummary = Summary.winsAnalysisWithHtmlReport("Man United");
 
 manUnitedSummary.buildAndPrintReport(matchReader.matches);
